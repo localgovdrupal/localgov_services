efc21e036006dc78bb8d50a1204d11623d2f8ca0
ac <ac@example.com>
2018-04-18 14:38:32 +0100

    Creating lazy loader base class

---
+++ modules/bhcc_service_hub/src/LazyLoader.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\bhcc_service_hub;
+
+use Drupal\bhcc_helper\LazyLoaderBase;
+use Drupal\bhcc_helper\LazyLoaderInterface;
+
+/**
+ * Class LazyLoader.
+ *
+ * Provides lazy loader functions for service hub pages.
+ *
+ * @package Drupal\bhcc_service_hub
+ */
+class LazyLoader extends LazyLoaderBase implements LazyLoaderInterface {
+
+  public function getType() {
+    return 'service_hub';
+  }
+}
