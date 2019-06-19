8c6d1d5db9e0696e0892c1e889b97a0d69b61bf2
Adnan <adnan.muhammad@croydon.gov.uk>
2019-06-19 17:57:37 +0100

    Initial commit.
    Added BHCC custom modules and themes to an up-to-date composer project template.

---
+++ src/Plugin/Field/FieldWidget/LinkWithType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Drupal\bhcc_helper\Plugin\Field\FieldWidget;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\WidgetBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\link\Plugin\Field\FieldWidget\LinkWidget;
+
+/**
+ * Plugin implementation of the 'link' widget.
+ *
+ * @FieldWidget(
+ *   id = "link_with_type",
+ *   label = @Translation("Link (with type)"),
+ *   field_types = {
+ *     "link"
+ *   }
+ * )
+ */
+class LinkWithType extends LinkWidget {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    $element = parent::formElement($items, $delta, $element, $form, $form_state);
+
+    $element['options']['type'] = [
+      '#title' => $this->t('Type'),
+      '#default_value' => isset($items[$delta]->options['type']) ? $items[$delta]->options['type'] : 'action',
+      '#type' => 'select',
+      '#options' => [
+        'action' => 'Action',
+        'basic' => 'Information'
+      ]
+    ];
+
+    return $element;
+  }
+}
