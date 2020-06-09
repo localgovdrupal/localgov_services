605e12538a0bfb047e6b81d90f1ad02aa42c6f09
Stephen Cox <stephen@agile.coop>
2020-06-09 15:17:26 +0100

    Fix failing block tests. #41

--- tests/src/Functional/ServicesBlockTest.php
+++ tests/src/Functional/ServicesBlockTest.php
@@ -2,12 +2,17 @@
 
 namespace Drupal\Tests\localgov_services\Functional;
 
+use Drupal\node\NodeInterface;
+use Drupal\taxonomy\Entity\Term;
 use Drupal\Tests\BrowserTestBase;
+use Drupal\Tests\node\Traits\NodeCreationTrait;
 
 /**
  * Functional tests for LocalGovDrupal install profile.
  */
 class ServicesBlockTest extends BrowserTestBase {
+
+  use NodeCreationTrait;
 
   /**
    * {@inheritdoc}
@@ -29,92 +34,77 @@ class ServicesBlockTest extends BrowserTestBase {
   ];
 
   /**
-   * {@inheritdoc}
-   */
-  protected function setUp() {
-    parent::setUp();
-
-    $this->adminUser = $this->drupalCreateUser([
-      'access content',
-      'administer nodes',
-      'bypass node access',
-      'create localgov_services_landing content',
-      'create localgov_services_page content',
-      'create localgov_services_sublanding content',
-      'create terms in topic',
-      'edit own localgov_services_landing content',
-      'edit own localgov_services_page content',
-      'edit own localgov_services_sublanding content',
-    ]);
-  }
-
-  /**
    * Test blocks display.
    */
   public function testServiceCoreBlocksDisplay() {
-    $this->drupalLogin($this->adminUser);
 
     // Check Services CTA block show on landing pages with external link.
-    $edit = [
-      'title[0][value]' => 'Test landing page',
-      'body[0][summary]' => 'Test landing summary',
-      'body[0][value]' => 'Test landing text',
-      'status[value]' => 1,
-      'field_common_tasks[0][uri]' => 'https://example.com/',
-      'field_common_tasks[0][title]' => 'Example button text',
-    ];
-    $this->drupalPostForm('/node/add/localgov_services_landing', $edit, 'Save');
+    $landing_path = '/' . $this->randomMachineName(8);
+    $this->createNode([
+      'type' => 'localgov_services_landing',
+      'title' => 'Test landing page',
+      'body' => [
+        'summary' => 'Test landing summary',
+        'value' => 'Test landing text',
+      ],
+      'status' => NodeInterface::PUBLISHED,
+      'field_common_tasks' => [
+        'uri' => 'https://example.com/',
+        'title' => 'Example button text',
+      ],
+      'path' => ['alias' => $landing_path],
+    ]);
     $this->drupalGet('/node/1');
     $this->assertSession()->responseContains('href="https://example.com/"');
     $this->assertSession()->pageTextContains('Example button text');
 
     // Check node title and summary display on service page with internal link.
-    $edit = [
-      'title[0][value]' => 'Test services page',
-      'body[0][summary]' => 'Test services summary text',
-      'body[0][value]' => 'Test services body text',
-      'status[value]' => 1,
-      'field_common_tasks[0][uri]' => 'Test landing page (1)',
-      'field_common_tasks[0][title]' => 'Landing page link',
-    ];
-    $this->drupalPostForm('/node/add/localgov_services_page', $edit, 'Save');
+    $services_page = $this->createNode([
+      'type' => 'localgov_services_page',
+      'title' => 'Test services page',
+      'body' => [
+        'summary' => 'Test services page summary',
+        'value' => 'Test services page text',
+      ],
+      'status' => NodeInterface::PUBLISHED,
+      'field_common_tasks' => [
+        'uri' => 'internal:' . $landing_path,
+        'title' => 'Landing page link',
+      ],
+    ]);
     $this->drupalGet('/node/2');
-    $this->assertSession()->responseContains('href="/node/1"');
+    $this->assertSession()->responseContains('href="' . $landing_path . '"');
     $this->assertSession()->pageTextContains('Landing page link');
 
     // Check manually added related links.
     $this->assertSession()->pageTextNotContains('Related Links');
-    $edit = [
-      'field_override_related_links[value]' => 1,
-      'field_related_links[0][uri]' => 'http://test.com/',
-      'field_related_links[0][title]' => 'Example related link',
-    ];
-    $this->drupalPostForm('/node/2/edit', $edit, 'Save');
+    $services_page->set('field_override_related_links', ['value' => 1]);
+    $services_page->set('field_related_links', [
+      'uri' => 'http://test.com/',
+      'title' => 'Example related link',
+    ]);
+    $services_page->save();
     $this->drupalGet('/node/2');
     $this->assertSession()->pageTextContains('Related Links');
     $this->assertSession()->responseContains('href="http://test.com/"');
     $this->assertSession()->pageTextContains('Example related link');
 
     // Check related topics.
-    $topic_name = $this->randomMachineName(8);
-    $edit = [
-      'name[0][value]' => $topic_name,
-      'status[value]' => 1,
-    ];
-    $this->drupalPostForm('/admin/structure/taxonomy/manage/topic/add', $edit, 'Save');
     $this->assertSession()->pageTextNotContains('Related Topics');
-    $edit = [
-      'field_topic_term[target_id]' => $topic_name . ' (1)',
-      'field_hide_related_topics[value]' => 0,
-    ];
-    $this->drupalPostForm('/node/2/edit', $edit, 'Save');
+    $topic_name = $this->randomMachineName(8);
+    $topic = Term::create([
+      'name' => $topic_name,
+      'vid' => 'topic',
+    ]);
+    $topic->save();
+    $services_page->set('field_topic_term', ['target_id' => $topic->id()]);
+    $services_page->set('field_hide_related_topics', ['value' => 0]);
+    $services_page->save();
     $this->drupalGet('/node/2');
     $this->assertSession()->pageTextContains('Related Topics');
     $this->assertSession()->pageTextContains($topic_name);
-    $edit = [
-      'field_hide_related_topics[value]' => 1,
-    ];
-    $this->drupalPostForm('/node/2/edit', $edit, 'Save');
+    $services_page->set('field_hide_related_topics', ['value' => 1]);
+    $services_page->save();
     $this->drupalGet('/node/2');
     $this->assertSession()->pageTextNotContains('Related Topics');
     $this->assertSession()->pageTextNotContains($topic_name);
