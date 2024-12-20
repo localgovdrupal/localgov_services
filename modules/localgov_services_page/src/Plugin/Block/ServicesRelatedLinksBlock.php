480808cc7d77501710ed0cf9acaccf04eb95684c
Andy Broomfield <talk@andybroomfield.com>
2024-12-20 16:00:58 +0000

    Remove the override releated links from service pages
    Fix #72
    
    This is a holdover from the old BHCC site, but the functionlity for
    automated links was never implemented in Localgov Drupal.
    
    This will remove this field for new sites, and will always render the
    releated links if they are present regardless.

--- modules/localgov_services_page/src/Plugin/Block/ServicesRelatedLinksBlock.php
+++ modules/localgov_services_page/src/Plugin/Block/ServicesRelatedLinksBlock.php
@@ -5,8 +5,6 @@ namespace Drupal\localgov_services_page\Plugin\Block;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Url;
 use Drupal\localgov_services\Plugin\Block\ServicesBlockBase;
-use Drupal\node\Entity\Node;
-use Drupal\taxonomy\TermInterface;
 
 /**
  * Provides a 'Services Related Links Block' block.
@@ -26,7 +24,7 @@ class ServicesRelatedLinksBlock extends ServicesBlockBase implements ContainerFa
   public function build() {
     $build = [];
 
-    $links = $this->getShouldUseManual() ? $this->buildManual() : $this->buildAutomated();
+    $links = $this->getLinks();
 
     if ($links) {
       $build[] = [
@@ -44,7 +42,7 @@ class ServicesRelatedLinksBlock extends ServicesBlockBase implements ContainerFa
    * @return array
    *   Array of links.
    */
-  private function buildManual() {
+  private function getLinks() {
     $links = [];
 
     if ($this->node->hasField('localgov_related_links')) {
@@ -59,98 +57,6 @@ class ServicesRelatedLinksBlock extends ServicesBlockBase implements ContainerFa
     }
 
     return $links;
-  }
-
-  /**
-   * Automatically builds a list of links based on the most relevant pages.
-   *
-   * @todo Decide how the automated link generation should work.
-   *
-   * @return array
-   *   Array of links.
-   */
-  private function buildAutomated() {
-    // Return an empty array for the time being.
-    return [];
-
-    // @codingStandardsIgnoreStart
-    // Convert topics field into an array we can use in the query.
-    $topics = [];
-
-    foreach ($this->getTopics() as $topic) {
-      $topics[] = $topic->id();
-    }
-
-    if ($topics) {
-      // Perform our query.
-      $query = $this->database->query('SELECT entity_id FROM node__localgov_topic_classified
-  LEFT JOIN node_field_data ON node_field_data.nid=node__localgov_topic_classified.entity_id
-  WHERE node__localgov_topic_classified.entity_id != :nid
-  AND node__localgov_topic_classified.localgov_topic_classified_target_id IN (:tids[])
-  AND node_field_data.status=1
-  GROUP BY node__localgov_topic_classified.entity_id
-  ORDER BY count(*) desc
-  LIMIT 6;',
-        [
-          ':nid' => $this->node->id(),
-          ':tids[]' => $topics,
-        ]
-      );
-
-      $list = [];
-      foreach ($query->fetchAll() as $result) {
-        $node = Node::load($result->entity_id);
-        $list[] = [
-          'title' => $node->getTitle(),
-          'url' => $node->toUrl(),
-        ];
-      }
-
-      return $list;
-    }
-
-    return [];
-    // @codingStandardsIgnoreEnd
-  }
-
-  /**
-   * Decide if we should use a manual override.
-   *
-   * @return bool
-   *   Should manual links be displayed?
-   */
-  private function getShouldUseManual() {
-    if ($this->node->hasField('localgov_override_related_links') && !$this->node->get('localgov_override_related_links')->isEmpty()) {
-      return $this->node->get('localgov_override_related_links')->first()->getValue()['value'];
-    }
-
-    return FALSE;
-  }
-
-  /**
-   * Build links array for the related topics block.
-   *
-   * @return array
-   *   Array of topics.
-   */
-  private function getTopics() {
-    $topics = [];
-
-    if ($this->node->hasField('localgov_topic_classified')) {
-
-      /** @var \Drupal\taxonomy\TermInterface $term_info */
-      foreach ($this->node->get('localgov_topic_classified')->getValue() as $term_info) {
-        $topicEntity = $this->entityTypeManager->getStorage('taxonomy_term')->load($term_info['target_id']);
-
-        // Add topic only if an actual taxonomy term,
-        // deleted topics can return NULL if still present on the node.
-        if ($topicEntity instanceof TermInterface) {
-          $topics[] = $topicEntity;
-        }
-      }
-    }
-
-    return $topics;
   }
 
 }
