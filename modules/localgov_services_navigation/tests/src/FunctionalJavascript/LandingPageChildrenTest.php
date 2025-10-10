a917be7cf2c4995e61615a917a5bee131be136ee
Finn <finn@opencode.uk>
2025-10-10 08:53:06 +0100

    Update test to explore if we need to load the path directly from the node.

--- modules/localgov_services_navigation/tests/src/FunctionalJavascript/LandingPageChildrenTest.php
+++ modules/localgov_services_navigation/tests/src/FunctionalJavascript/LandingPageChildrenTest.php
@@ -146,7 +146,7 @@ class LandingPageChildrenTest extends WebDriverTestBase {
     $target = $page->find('css', '#edit-localgov-common-tasks-1-uri');
     $drag->dragTo($target);
     // Shouldn't overwrite.
-    $assert_session->fieldValueEquals('edit-localgov-common-tasks-1-uri', '/foo');
+    $assert_session->fieldValueEquals('edit-localgov-common-tasks-1-uri', $child[2]->toUrl()->toString());
     $assert_session->fieldValueEquals('edit-localgov-common-tasks-1-title', '\'; #child_2\n');
 
   }
